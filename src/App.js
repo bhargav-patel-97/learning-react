1a6e3836120e3088dabcdf6bef8e6b563556ab07
bhargav <bhargavpatel6040@gmail.com>
2020-02-02 19:51:32 +0530

    Adding react state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Bhargav', age: 22 },
+      { name: 'Nihar', age: 20 },
+      { name: 'Abhi', age:19 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    //DONT DO THIS this.state.persons[0] = 'Bhargav Patel';
+    // INSTEAD, USE setState METHOD
+    this.setState({
+      persons: [
+        { name: 'Pratik', age: 22 },
+        { name: 'Arjun', age: 21 },
+        { name: 'Hit', age:22 }
+      ]
+    });
+    } 
+
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
-        <p className="App-intro">
-          Hi! I am Bhargav Patel.
-        </p>
-        <Person name="Bhargav" age="22"/>
-        <Person name="Nihar" age="20"/>
-        <Person name="Abhi" age="19"/>
+        <h1 className="App-title">Welcome to React</h1>
+        <button onClick={this.switchNameHandler}>Switch Names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobby is JavaScript</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?',));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,8 @@ import React from'react';
 const person = (props) => {
     return (
     <div>
-<p>I am {props.name} and I'm {props.age} years old.</p>
+        <p>I am {props.name} and I'm {props.age} years old.</p>
+        <p>{props.children}</p>
     </div>
     );
 };
