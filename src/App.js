83c60847477685f3e29eb4088cbdee0352c7bee2
bhargav <bhargavpatel6040@gmail.com>
2020-04-04 20:13:27 +0530

    Removed changeNames method and Added Toggle Method

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = () => {
-  const [ personsState, setPersonsState ] = useState({
+class app extends Component {
+
+state = {
     persons: [
       { name: 'Bhargav', age: 22 },
       { name: 'Nihar', age: 20 },
       { name: 'Abhi', age:19 }
-    ]
-  });
+    ],
+    displayPersons: false
+  }
 
-  const switchNameHandler = () => {
-    //DONT DO THIS this.state.persons[0] = 'Bhargav Patel';
-    // INSTEAD, USE setState METHOD
-    setPersonsState({
-      persons: [
-        { name: 'Pratik', age: 22 },
-        { name: 'Arjun', age: 21 },
-        { name: 'Hit', age:22 }
-      ]
-    });
-    };
-
-    const handleChange = (event) => {
-      setPersonsState({
+    handleChange = (event) => {
+      this.setState({
         persons: [
           { name: 'Pratik', age: 22 },
           { name: event.target.value, age: 21 },
           { name: 'Hit', age:22 }
         ]
       });
-    };
+    }
 
+    togglePersonHandle = () => {
+      const display = this.state.displayPersons;
+      this.setState({displayPersons: !display});
+    }
+
+  render () {
     return (
       <div className="App">
         <h1 className="App-title">Welcome to React</h1>
-        <button className="Button" onClick={switchNameHandler}>Switch Names</button>
-        
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person 
-        name={personsState.persons[1].name} 
-        age={personsState.persons[1].age}
-        change={handleChange}
-        />
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <button className="Button" onClick={this.togglePersonHandle}>Toggle</button>
+        { this.state.displayPersons ?
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              change={this.handleChange}
+            />
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          </div> : null
+        }
       </div>
-    );
+    )
+  };
 }
 
 export default app;
